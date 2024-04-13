b873dda4ad7d8bae810dcc3c8db757f5cf9e5b56
Tyler B <typh15@yahoo.com>
2024-04-13 04:37:41 -0400

    Random Map Gen
    Now has random sample data and adjacency data for each room

--- Maps/MiniMapBlocks/RoomLoad.gd
+++ Maps/MiniMapBlocks/RoomLoad.gd
@@ -1,12 +1,17 @@
 extends Node2D
 
 var _position = Vector2()
+var color_param = int()
 
 func _ready():
-	init(_position)
+	init(_position, color_param)
 	
-func init(_position):
-	position = _position
+func init(_position, color_param):
+	position = _position*33
+	if _position == Vector2(0,0):
+		$Polygon2D.color = Color(0, 0, 0, 1)
+	else:
+		$Polygon2D.color = Color(color_param*0.1, -color_param*0.1, color_param*0.1, color_param*0.1)
 	var img_size = $Sprite.texture.get_size().x / 2
 	$Sprite.scale = Vector2(1, 1) * 0.5
 	
