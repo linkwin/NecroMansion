35b1b937399af35d4ca9db61f494e26ee8b2643c
Tyler B <typh15@yahoo.com>
2024-04-13 12:59:58 -0400

    More Better Random

--- Maps/Map Layout Generator.gd
+++ Maps/Map Layout Generator.gd
@@ -2,10 +2,10 @@ extends Area2D
 
 
 var ARoom = preload("res://Maps/MiniMapBlocks/Rooms.tscn")
-
-var init_seed := hash("Carrots")
-
+var init_seed := hash("Carrotds")
 var directions := [Vector2(1, 0), Vector2(-1, 0), Vector2(0, 1), Vector2(0, -1)]
+
+
 
 
 func array_mult(in_array, inger):
@@ -16,21 +16,21 @@ func array_mult(in_array, inger):
 			out_array.append(j)
 	return(out_array)
 
-func spawn_room(_position=null, color_param=1):
+func spawn_room(_position, color_param=1):
 	var c = ARoom.instance()
 	add_child(c)
 	c.init(_position, color_param)
-	
-	
-	
-	
+
+
+
+
 # prob_dist will have the relative prob. of each occurence 
 # per room-property per room
 # Example: prob_dists = [ [[1, 4], [1, 3, 3, 3]], [[1, 1], [2, 0, 6, 3]]] , [ [[ ...
 #                       (Room 1-  (Property 1- ((Rel Prob 1, Rel Prob 2), ...), ... )    
-# Need to be sure these arrays match in size with data_pts_per_room list
+# Need to be sure these arrays match in size
 
-func room_data_load(map, initial_seed, data_pts_per_room, prob_dists):
+func room_data_load(map, initial_seed, prob_dists):
 	var room_prop_rng = RandomNumberGenerator.new()
 	var map_data := []
 	room_prop_rng.seed = initial_seed + 200
@@ -51,30 +51,29 @@ func room_data_load(map, initial_seed, data_pts_per_room, prob_dists):
 		i += 20
 		map_data.append(room_data)
 	print("MAP DATA: ", map_data)
-
-
-
-
-
+	return(map_data)
 
 
 
 
 func navigation_load(map, directions):
 	var map_nav := []
-	var poss_dir := []
 	for room in map:
+		var poss_dir := []
 		for dir in directions:
 			if room+dir in map:
 				poss_dir.append(dir)
 		map_nav.append(poss_dir)
-	print("DOOR DIRECTIONS: ", map_nav)
+	#print("DOOR DIRECTIONS: ", map_nav)
+	return(map_nav)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var room_rng = RandomNumberGenerator.new()
-	var num_rooms := 6
+	var num_rooms := 15
 	var map := []
+	var map_navi := []
+	var map_datas := []
 	room_rng.seed = init_seed
 	var current_position = Vector2(0,0)
 	var new_direction = Vector2()
@@ -89,8 +88,8 @@ func _ready():
 		i += 1
 	print("MAP COORDINATES: ", map)
 	print("")
-	navigation_load(map, directions)
-	room_data_load(map, init_seed, 2, array_mult([[[1, 1], [1, 2]]], 6) )
+	map_navi = navigation_load(map, directions)
+	map_datas = room_data_load(map, init_seed, array_mult([[[1, 1], [1, 2]]], num_rooms) )
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-#	pass
+#	pass[    [[5 3 2], [10 4 1], [1 1 1 1 1 1], ... 
